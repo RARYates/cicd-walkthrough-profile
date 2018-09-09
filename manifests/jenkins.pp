64f24a1b8dfdd705eeee1c072c73140ebeb958ea
Ryan Russell-Yates <ryan.russellyates@gmail.com>
2018-09-09 04:29:05 +0000

    initial commit

---
+++ manifests/jenkins.pp
@@ -0,0 +1,19 @@
+class profile::jenkins {
+
+  class { 'jenkins': lts => true }
+
+  package {'git': ensure => latest }
+
+  file {'/tmp/pdk.rpm':
+    ensure => file,
+    source => 'https://puppet-pdk.s3.amazonaws.com/pdk/1.7.0.0/repos/el/7/puppet5/x86_64/pdk-1.7.0.0-1.el7.x86_64.rpm',
+  }
+
+# Install latest PDK directly from Puppet Source
+  package {'pdk':
+    ensure => installed,
+    source => '/tmp/pdk.rpm',
+    require => File['/tmp/pdk.rpm'],
+  }
+
+}
